6c2bc6fbc2cf7aebfa7d1c534dfda278c6c9ebda
edfefegeger <silov155@gmail.com>
2023-12-10 20:16:44 +0300

    correct congig from txt, and it's all

--- model.py
+++ model.py
@@ -6,157 +6,134 @@ from functools import wraps
 import numpy as np
 import queue
 import sounddevice as sd
-import threading
-import whisper
 from time import time as now
+import whisper
 import configparser
-import soundfile as sf
 
 FLAGS = flags.FLAGS
 
-# Определение флагов с комментариями
-flags.DEFINE_string('model_name', 'base.en', 'Версия модели OpenAI Whisper для использования.')
-flags.DEFINE_string('language', 'en', 'Язык для использования или пусто для автоопределения.')
-flags.DEFINE_string('input_device', 'default', 'Устройство ввода для записи аудио.')
-
-flags.DEFINE_integer('sample_rate', 16000, 'Частота дискретизации записываемого аудио.')
-flags.DEFINE_integer('num_channels', 1, 'Количество каналов записываемого аудио.')
-flags.DEFINE_integer('channel_index', 0, 'Индекс канала для транскрипции.')
-flags.DEFINE_integer('chunk_seconds', 10, 'Длина в секундах каждого фрагмента записанного аудио.')
-flags.DEFINE_string('latency', 'low', 'Задержка потока записи.')
-
-flags.DEFINE_float('confidence_threshold', 0.5, 'Порог уверенности для транскрипции Whisper.')
+# Определение настроек
+file1 = open("settings.ini", "r")
+line = file1.readline()
+model_file = file1.readline()
+language_file = file1.readline()
+confidence_file = file1.readline()
+processing_interval_file = file1.readline()
+timeout_file = file1.readline()
 
 # Путь к файлу настроек
 flags.DEFINE_string('settings_file', 'settings.ini', 'Путь к файлу настроек.')
 
-# Декоратор для измерения времени выполнения функций
+flags.DEFINE_string('model_name', model_file.strip(),
+                    'The version of the OpenAI Whisper model to use.')
+flags.DEFINE_string('language', language_file.strip(),
+                    'The language to use or empty to auto-detect.')
+flags.DEFINE_string('input_device', 'default', 'The input device used to record audio.')
+
+confidence_value = confidence_file.split('=')[-1].strip()
+flags.DEFINE_float('confidence', float(confidence_value), 'Минимальная уверенность для использования.')
+
+flags.DEFINE_integer('processing_interval', processing_interval_file.strip(), 'Интервал обработки аудио в секундах.')
+
+flags.DEFINE_integer('timeout', timeout_file.strip(), 'Таймаут работы приложения в секундах.')
+
+flags.DEFINE_integer('sample_rate', 16000,
+                     'The sample rate of the recorded audio.')
+flags.DEFINE_integer('num_channels', 1,
+                     'The number of channels of the recorded audio.')
+flags.DEFINE_integer('channel_index', 0,
+                     'The index of the channel to use for transcription.')
+flags.DEFINE_integer('chunk_seconds', 10,
+                     'The length in seconds of each recorded chunk of audio.')
+flags.DEFINE_string('latency', 'low', 'The latency of the recording stream.')
+
+
+# A decorator to log the timing of performance-critical functions.
 def timed(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = now()
         result = func(*args, **kwargs)
         stop = now()
-        logging.debug(f'{func.__name__} заняла {stop-start:.3f} сек.')
+        logging.debug(f'{func.__name__} took {stop-start:.3f}s')
         return result
     return wrapper
 
-# Функция для транскрипции аудио
+
 @timed
-def transcribe(model, audio, confidence_threshold):
-    # Запуск модели Whisper для транскрипции аудио.
+def transcribe(model, audio):
+    # Run the Whisper model to transcribe the audio chunk.
     result = whisper.transcribe(model=model, audio=audio)
 
-    # Использование транскрибированного текста, если уверенность превышает порог.
-    confidence = result.get('confidence', 0.0)
-    if confidence >= confidence_threshold:
-        text = result['text'].strip()
-        logging.info(text)
+    # Use the transcribed text.
+    text = result['text'].strip()
+    logging.info(text)
 
-# Функция для обработки аудио
-def process_audio(audio_queue, model, confidence_threshold):
-    while True:
-        # Блокировка до появления следующего фрагмента аудио в очереди.
-        audio = audio_queue.get()
 
-        # Транскрипция последнего фрагмента аудио.
-        transcribe(model=model, audio=audio, confidence_threshold=confidence_threshold)
+@timed
+def stream_callback(indata, frames, time, status, audio_queue):
+    if status:
+        logging.warning(f'Stream callback status: {status}')
 
-# Глобальные переменные для управления записью аудио
-recording_chunk = None
-recording_start_time = 0
-user_speaking = False
-queue_audio = queue.Queue()  # Добавим глобальную очередь для передачи в record_audio
+    # Add this chunk of audio to the queue.
+    audio = indata[:, FLAGS.channel_index].copy()
+    audio_queue.put(audio)
 
-# Функция для записи аудио
-def record_audio(callback, block_size, queue_audio, **kwargs):
-    global recording_chunk, recording_start_time, user_speaking
 
-    with sd.InputStream(callback=callback, blocksize=block_size, **kwargs):
-        recording_chunk = np.zeros(block_size, dtype=np.float32)
-        recording_start_time = now()
-        user_speaking = False
+@timed
+def process_audio(audio_queue, model):
+    # Block until the next chunk of audio is available on the queue.
+    audio = audio_queue.get()
 
-        print("Поток записи аудио запущен.")
+    # Transcribe the latest audio chunk.
+    transcribe(model=model, audio=audio)
 
-        while True:
-            sd.sleep(10)
 
-            if now() - recording_start_time > 10:
-                if user_speaking:
-                    queue_audio.put(recording_chunk.copy())
-                    print("Голос был записан в очередь для распознавания текста.")
-                    user_speaking = False
-                recording_chunk = np.zeros(block_size, dtype=np.float32)
-                recording_start_time = now()
-
-            if recording_chunk is not None:
-                volume_level = np.max(np.abs(recording_chunk))
-                if volume_level > 0.1:
-                    user_speaking = True
-                    print("Голос был обнаружен.")
-
-        print("Поток записи аудио завершен.")
-
-# Основная функция
 def main(argv):
     # Загрузка настроек из файла конфигурации
-    logging.info(f'Загрузка настроек из {FLAGS.settings_file}...')
+    logging.info(f'Loading settings from {FLAGS.settings_file}...')
 
     # Используем библиотеку configparser для загрузки настроек из файла
     config = configparser.ConfigParser()
-    config.read(FLAGS.settings_file)
+   
 
-    # Устанавливаем значения флагов из раздела DEFAULT
-    if config.has_section('DEFAULT'):
-        for option in config.options('DEFAULT'):
+    # Устанавливаем значения флагов из конфигурационного файла
+    for section in config.sections():
+        for option in config.options(section):
             if hasattr(FLAGS, option):
-                # Установка значений, преобразуя типы данных при необходимости
                 flag_value = getattr(FLAGS, option)
+                # Установка значений, преобразуя типы данных при необходимости
                 if isinstance(flag_value, int):
-                    setattr(FLAGS, option, config.getint('DEFAULT', option))
+                    setattr(FLAGS, option, config.getint(section, option))
                 elif isinstance(flag_value, float):
-                    setattr(FLAGS, option, config.getfloat('DEFAULT', option))
+                    setattr(FLAGS, option, config.getfloat(section, option))
                 else:
-                    setattr(FLAGS, option, config.get('DEFAULT', option))
+                    setattr(FLAGS, option, config.get(section, option))
 
-    # Загрузка модели Whisper в память, скачивание, если необходимо.
-    logging.info(f'Загрузка модели "{FLAGS.model_name}"...')
+    # Загрузка модели Whisper
+    logging.info(f'Loading model "{FLAGS.model_name}"...')
     model = whisper.load_model(name=FLAGS.model_name)
 
-    # Первый запуск модели медленный (инициализация буфера), поэтому запускаем его один раз без аудио.
-    logging.info('Подготовка модели...')
+    # Предварительный прогон модели (warm-up)
+    logging.info('Warming model up...')
     block_size = FLAGS.chunk_seconds * FLAGS.sample_rate
-    whisper.transcribe(model=model, audio=np.zeros(block_size, dtype=np.float32))
+    whisper.transcribe(model=model,
+                       audio=np.zeros(block_size, dtype=np.float32))
 
-    # Запуск потока для обработки фрагментов аудио
+    # Начало потока аудио и обработка
+    logging.info('Starting stream...')
     audio_queue = queue.Queue()
-    audio_processing_thread = threading.Thread(target=process_audio, args=(audio_queue, model, FLAGS.confidence_threshold))
-    audio_processing_thread.daemon = True
-    audio_processing_thread.start()
-
-    # Запуск потока для записи аудио
     callback = partial(stream_callback, audio_queue=audio_queue)
-    recording_thread = threading.Thread(target=record_audio, args=(callback, block_size, queue_audio), kwargs={'samplerate': FLAGS.sample_rate, 'device': FLAGS.input_device, 'channels': FLAGS.num_channels, 'dtype': np.float32, 'latency': FLAGS.latency})
-
-    recording_thread.start()
-
-    print("Программа запущена. Ожидание завершения работы...")
-
-    # Ожидание завершения обоих потоков
-    recording_thread.join()
-    audio_processing_thread.join()
-
-    print("Программа завершена.")
-
-# Функция обратного вызова для потока записи
-def stream_callback(indata, frames, time, status, audio_queue):
-    if status:
-        logging.warning(f'Статус обратного вызова потока: {status}')
-
-    # Добавление этого фрагмента аудио в очередь.
-    audio = indata[:, FLAGS.channel_index].copy()
-    audio_queue.put(audio)
+    with sd.InputStream(samplerate=FLAGS.sample_rate,
+                        blocksize=block_size,
+                        device=FLAGS.input_device,
+                        channels=FLAGS.num_channels,
+                        dtype=np.float32,
+                        latency=FLAGS.latency,
+                        callback=callback):
+        while True:
+            # Обработка блоков аудио из очереди
+            process_audio(audio_queue, model)
 
 if __name__ == '__main__':
     app.run(main)
